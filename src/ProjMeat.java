b4ce846d67e38285fa85d65708bffb3d4df2ff36
Gabriel Fragas <gabrielx672@hotmail.com>
2012-10-06 02:37:11 -0300

    Base para implementação do Projectile Meat
    Classe ProjMeat foi alterada para uma versão base, para que a física do
    projétil possa ser implementada.

--- src/ProjMeat.java
+++ src/ProjMeat.java
@@ -3,15 +3,26 @@ import java.awt.Graphics2D;
 
 
 public class ProjMeat extends Projectile {
+	
+	float flightTime;
+	float projectileStartPositionX;
+	float projectileStartPositionY;
+	float projectileVelocityX = 100;
+	float projectileVelocityY = 100;
+	float projectileRotation;
 
 	public ProjMeat(float x, float y, float velX, float velY, Object pai) {
 		super(x, y, velX, velY, pai);
-		// TODO Auto-generated constructor stub
+	}
+	
+	public void selfSimulates(long diffTime) {
+		x += velX*diffTime/1000.0f;
+		y += velY*diffTime/1000.0f;
 	}
 
 	@Override
 	public void selfDraws(Graphics2D dbg, int mapX, int mapY) {
 		dbg.setColor(Color.RED);
-		dbg.fillOval((int)(x-mapX-2), (int)(y-mapY-2), 4, 4);
+		dbg.fillOval((int)(x-mapX-2), (int)(y-mapY-2), 8, 8);
 	}
 }
