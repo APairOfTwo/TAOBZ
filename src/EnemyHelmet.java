d59411ec7b3a2825c5e2c24d79225c34f8038520
Guilherme Ouriques <guilherme.nro@gmail.com>
2012-10-10 01:12:02 -0300

    Classes dos inimigos Vegetariano e Helmet criadas
    Só pra facilitar e lembrar.

---
+++ src/EnemyHelmet.java
@@ -0,0 +1,14 @@
+import java.awt.image.BufferedImage;
+
+
+public class EnemyHelmet extends Character {
+
+	public EnemyHelmet(float x, float y, BufferedImage charset, int charsetX,
+			int charsetY, int frameWidth, int frameHeight, int numberOfFrames,
+			int charsetWidth, int charsetHeight) {
+		super(x, y, charset, charsetX, charsetY, frameWidth, frameHeight,
+				numberOfFrames, charsetWidth, charsetHeight);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/EnemyVegetarian.java
@@ -0,0 +1,14 @@
+import java.awt.image.BufferedImage;
+
+
+public class EnemyVegetarian extends Character {
+
+	public EnemyVegetarian(float x, float y, BufferedImage charset,
+			int charsetX, int charsetY, int frameWidth, int frameHeight,
+			int numberOfFrames, int charsetWidth, int charsetHeight) {
+		super(x, y, charset, charsetX, charsetY, frameWidth, frameHeight,
+				numberOfFrames, charsetWidth, charsetHeight);
+		// TODO Auto-generated constructor stub
+	}
+
+}
