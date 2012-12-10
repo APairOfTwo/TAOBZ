cb1149a93e57c255cb7b3a3bf46d0431c28086d8
Gabriel Fragas <gabrielx672@hotmail.com>
2012-12-09 22:08:40 -0200

    Ícone no Checkpoint e mensagem exibida

--- src/Checkpoint.java
+++ src/Checkpoint.java
@@ -1,5 +1,5 @@
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 
 public class Checkpoint extends Sprite {
@@ -7,6 +7,13 @@ public class Checkpoint extends Sprite {
 	boolean isActive;
 	boolean hasBeenActived = false;
 	boolean respawnBilly, respawnZombie;
+	BufferedImage checkpointOff = GamePanel.loadImage("sprites/checkpointOff.png");
+	BufferedImage checkpointOn = GamePanel.loadImage("sprites/checkpointOn.png");
+	BufferedImage checkpointMsg = GamePanel.loadImage("sprites/checkpointMsg.png");
+	int msgX, msgY, msgWidth, msgHeight;
+	boolean showMsg = false;
+	long msgTime = 0;
+	int collidedCounter = 0;
 	
 	public Checkpoint(int x, int y) {
 		this.x = x;
@@ -14,12 +21,21 @@ public class Checkpoint extends Sprite {
 		this.isActive = false;
 		this.respawnBilly = false;
 		this.respawnZombie = false;
+		this.msgX = (x - (checkpointMsg.getWidth()/2)) + (checkpointOn.getWidth()/2);
+		this.msgY = y - (checkpointOn.getHeight());
+		this.msgWidth = (msgX + checkpointMsg.getWidth());
+		this.msgHeight = (msgY + checkpointMsg.getHeight());
 	}
 
 	@Override
 	public void selfSimulates(long diffTime) {
 		for(Character c : CanvasGame.heroes) {
 			if(c.getBounds().intersects(x-CanvasGame.map.MapX, y-CanvasGame.map.MapY, 16, 64)) {
+				collidedCounter = 1;
+				if(!isActive && collidedCounter == 1) {
+					showMsg = true;
+					collidedCounter = 0;
+				}
 				isActive = true;
 				if(GamePanel.isCoop) {
 					if(!CanvasGame.billy.isAlive) {
@@ -30,16 +46,27 @@ public class Checkpoint extends Sprite {
 				}
 			}
 		}
+		if(showMsg) {
+			msgTime += diffTime;
+			msgX -= 0.02;
+			msgY -= 0.02;
+			msgWidth -= 0.02;
+			msgHeight -= 0.02;
+		}
+		if(msgTime >= 2000) {
+			showMsg = false;
+		}
 	}
 
 	@Override
 	public void selfDraws(Graphics2D dbg, int mapX, int mapY) {
 		if(this.isActive) {
-			dbg.setColor(Color.GREEN);
-			dbg.fillRect(x-CanvasGame.map.MapX, y-CanvasGame.map.MapY, 16, 64);
+			dbg.drawImage(checkpointOn, (int)(x-mapX), (int)(y-mapY), (int)((x+checkpointOn.getWidth())-mapX), (int)((y+checkpointOn.getHeight())-mapY), 0, 0, checkpointOn.getWidth(), checkpointOn.getHeight(), null);
 		} else {
-			dbg.setColor(Color.YELLOW);
-			dbg.fillRect(x-CanvasGame.map.MapX, y-CanvasGame.map.MapY, 16, 64);
+			dbg.drawImage(checkpointOff, (int)(x-mapX), (int)(y-mapY), (int)((x+checkpointOff.getWidth())-mapX), (int)((y+checkpointOff.getHeight())-mapY), 0, 0, checkpointOff.getWidth(), checkpointOff.getHeight(), null);
+		}
+		if(showMsg) {
+			dbg.drawImage(checkpointMsg, (int)(msgX-mapX), (int)(msgY-mapY), (int)(msgWidth-mapX), (int)(msgHeight-mapY), 0, 0, checkpointMsg.getWidth(), checkpointMsg.getHeight(), null);
 		}
 	}
 }
